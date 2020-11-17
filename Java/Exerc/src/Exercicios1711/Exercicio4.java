f430cd8fd1816969110c4b4ecb6e75f2c7adac56
Mateus <mateusv@outlook.com.br>
2020-11-17 11:42:03 -0300

    Subindo exercícios dia 17-11

---
+++ Java/Exerc/src/Exercicios1711/Exercicio4.java
@@ -0,0 +1,27 @@
+package Exercicios1711;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+	public static void main (String [] args) {
+		int numero1=0, numero2=0, numero3=0;
+			Scanner read = new Scanner (System.in);
+			System.out.println("Programa que retorna o maior número dentre três números digitados!");
+			System.out.println("Digite o primeiro número: ");
+			numero1= read.nextInt();
+			System.out.println("Digite o primeiro número: ");
+			numero2= read.nextInt();
+			System.out.println("Digite o primeiro número: ");
+			numero3= read.nextInt();
+			if(numero1 > numero2 && numero1 > numero3) {
+				System.out.println("O maior número é "+numero1);
+			}
+			else if(numero2 > numero1 && numero2 > numero3) {
+				System.out.println("O maior número é "+numero2);
+			}
+			else if(numero3 > numero1 && numero3 > numero2) {
+				System.out.println("O maior número é "+numero3);
+			}
+			
+	}
+}
