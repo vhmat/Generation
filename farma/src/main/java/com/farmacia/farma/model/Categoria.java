b3a2f4a980bdce9102840527afb5b726f0aaedd5
Mateus <mateusv@outlook.com.br>
2020-12-09 23:04:40 -0300

    Atividade Live Code e Exercício Spring

---
+++ farma/src/main/java/com/farmacia/farma/model/Categoria.java
@@ -0,0 +1,44 @@
+package com.farmacia.farma.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@NotBlank
+	@Size(min = 2, max = 50)
+	private String descricao;
+	@NotNull
+	private boolean ativo;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+	
+}
---
+++ farma/src/test/java/com/farmacia/farma/FarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.farma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
