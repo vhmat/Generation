333d5e6e050e306b8b803ba8aa4de654aa2420c0
Mateus <mateusv@outlook.com.br>
2021-01-06 14:33:48 -0300

    Blog Pessoal

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/UserLogin.java
@@ -0,0 +1,34 @@
+package org.generation.blogPessoal.model;
+
+public class UserLogin {
+	private String nome;
+	private String usuario;
+	private String senha;
+	private String tolken;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getTolken() {
+		return tolken;
+	}
+	public void setTolken(String tolken) {
+		this.tolken = tolken;
+	}
+	
+	
+}
